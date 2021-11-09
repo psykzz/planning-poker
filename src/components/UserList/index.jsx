ff0558f17403cf9232b47775b12c139b00b2fd45
Matt Smith <matt.daemon660@gmail.com>
2021-11-09 11:17:50 +0000

    Add score averages

--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -2,11 +2,23 @@ import React from "react"
 
 import * as styles from "./userlist.module.css"
 
+const standardDeviation = (array) => {
+  const n = array.length
+  if(!n) return NaN;
+  const mean = array.reduce((a, b) => a + b) / n
+  return Math.sqrt(array.map(x => Math.pow(x - mean, 2)).reduce((a, b) => a + b) / n)
+}
+
 export const UserList = ({ me, users, scores }) => {
+  let lowest = React.useMemo(() => Math.min(...scores.map(score => score.score)), [scores]);
+  let highest = React.useMemo(() => Math.max(...scores.map(score => score.score)), [scores]);
+  let stddev = React.useMemo(() => standardDeviation(scores.map(score => score.score)), [scores]);
   const scoreByUser = {}
   scores?.forEach(score => {
     scoreByUser[score.user_id] = score
   })
+
+  const showScores = scores.length && scores.every(score => score.revealed);
 
   // Stops the user list jumping around
   users?.sort((a, b) => a.id.localeCompare(b.id))
@@ -52,6 +64,13 @@ export const UserList = ({ me, users, scores }) => {
       {users?.map(user => (
         <User key={user.id} user={user} />
       ))}
+
+      {!!showScores && <div className={styles.averages}>
+        <h3>Score Averages</h3>
+        <div>Highest: {highest}</div>
+        <div>Lowest: {lowest}</div>
+        <div>Std Dev: {stddev}</div>
+      </div>}
     </div>
   )
 }
