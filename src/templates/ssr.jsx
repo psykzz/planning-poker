e35aaa25422141d1fc91c2bd9d4eba5e899ed92a
Matt <psykzz@users.noreply.github.com>
2022-10-20 12:04:26 +0100

    Update ssr.jsx

--- src/templates/ssr.jsx
+++ src/templates/ssr.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Component for rendering, doing nothing special.
 const TestTemplate = ({ serverData }) => {
   return <>
     <pre>{JSON.stringify(serverData, null, 2)}</pre>
@@ -9,18 +10,27 @@ const TestTemplate = ({ serverData }) => {
 };
 
 export const getServerData = async () => {
+  // Fetch some data
   const data = await (
     await fetch('http://worldtimeapi.org/api/timezone/Europe/Dublin')
   ).json();
     
+  
+  // Process it
   const date = new Date(data.datetime);
   
+  // Return object with optional { props, headers, status } 
   return {
     props: {
       time: data.utc_datetime,
       hours: date.getHours(),
       minutes: date.getMinutes(),
     },
+    
+    // Add cache control headers
+    // max-age == browser
+    // s-maxage == shared cache (CDN, etc)
+    // state-while-revalidate == time to serve stale request
     headers: {
       'Cache-Control':
         'public, max-age=5, s-maxage=15, stale-while-revalidate=30',
