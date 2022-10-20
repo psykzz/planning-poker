c6a89abe5d59d31ae324ed27db138377ebd84c5f
Matt <psykzz@users.noreply.github.com>
2022-10-20 12:00:21 +0100

    cleaner format

--- src/templates/ssr.jsx
+++ src/templates/ssr.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
 const TestTemplate = ({ serverData }) => {
-  return <pre>{JSON.stringify(serverData, null, 2)}</pre>;
+  return <>
+    <pre>{JSON.stringify(serverData, null, 2)}</pre>
+    <hr />
+    <div style={{}}>{serverData.hours}:{serverData.minutes}</div>
+  </>;
 };
 
 export const getServerData = async () => {
   const data = await (
     await fetch('http://worldtimeapi.org/api/timezone/Europe/Dublin')
   ).json();
+    
+  const date = new Date(data.utc_datetime);
+  
   return {
     props: {
       time: data.utc_datetime,
+      hours: date.getHours(),
+      minutes: date.getMinutes(),
     },
     headers: {
       'Cache-Control':
