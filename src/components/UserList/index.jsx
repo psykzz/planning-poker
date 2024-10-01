4e2d29ccf7b69bb74142e0befb86e5b321310d5e
Matt <psykzz@users.noreply.github.com>
2024-10-01 16:10:17 +0100

    fix: try / catch

--- src/components/UserList/index.jsx
+++ src/components/UserList/index.jsx
@@ -56,7 +56,7 @@ export const UserList = ({ me, users, scores }) => {
   try {
     // Try to sort by score as well.
     users?.sort((a,b) => (scoreByUser?.[a.id] ?? 0) > (scoreByUser?.[b.id] ?? 0));
-  catch (e) { console.error(e); }
+  } catch (e) { console.error(e); }
   const resetName = () => {
     if (typeof window === 'undefined') {
       return;
